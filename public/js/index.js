5d95e3123c877564fb32547811c2ecdbec8f0117
alesiatants <alesiatantsiurenko@mail.ru>
2023-10-10 22:24:52 +0300

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+	/*document.getElementById('btn').addEventListener('click', ()=>{
+		alert("hello");
+	});*/
+	/*function toggleCancel(){
+		if(!document.getElementById('btn').classList.contains('collapsed')){
+			document.getElementById('btn').classList.toggle('act');
+		}
+	}*/
